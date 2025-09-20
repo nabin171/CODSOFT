43fab5a5bcc2bed6291f2f7b4ba28506088dfb32
Nabin Karki <karki0008@gmail.com>
2025-09-20 22:46:23 +0545

    navba added

---
+++ my-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="mt-4 flex items-center justify-around gap-80">
+      <div className="flex items-center gap-6 ">
+        <div className="relative w-16 h-16">
+          <div className="bg-purple-500 rounded-full w-full h-full"></div>
+          <div className="absolute inset-0 flex items-center justify-center font-bold text-white">
+            NK
+          </div>
+        </div>
+        <div className="font-bold  text-3xl">Nabin Karki</div>
+      </div>
+      <div className="flex gap-10">
+        <button className=" text-2xl bg-purple-500 p-3 rounded-2xl">
+          Home
+        </button>
+        <button className=" text-2xl">About</button>
+        <button className=" text-2xl">Skills</button>
+        <button className=" text-2xl">Projects</button>
+        <button className=" text-2xl">Resume</button>
+        <button className=" text-2xl">Contact</button>
+      </div>{" "}
+    </div>
+  );
+};
+
+export default Navbar;
